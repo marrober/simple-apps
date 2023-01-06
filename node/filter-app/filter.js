39c76c65df63252dd231303de29d4a0fbdaf5fb7
Mark Roberts <marrober@redhat.com>
2023-01-06 12:19:58 +0000

    adding filter

---
+++ node/filter-app/filter.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const sprintfJS = require('sprintf-js');
+var ip = require("ip");
+const app = express();
+
+var pathPrefix = process.env.PATH_PREFIX;
+
+var port = 8080;
+
+console.log("phase: setup", "This app target port : " + port);
+console.log("phase: setup", "This app ip address  : " + ip.address());
+
+console.log("Application is starting......");
+var counter = 0;
+
+app.get('/', (request, response) => {
+  counter++;
+  messageText = sprintfJS.sprintf("this ip address %-15s  %04d", ip.address(), counter);
+  console.log("phase: root", messageText);
+  response.send(messageText + request + "\n");
+});
+
+console.log("Listening on port " + port);
+app.listen(port, () => console.log("phase: setup", "Listening on port " + port));
