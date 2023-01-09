1d1d70a26798f5756fa2bbe3deb83e8490d9e4dd
Mark Roberts <marrober@redhat.com>
2023-01-09 16:39:25 +0000

    Update filter.js, layer.js, and 6 more files...

--- node/filter-app/filter.js
+++ node/filter-app/filter.js
@@ -30,7 +30,7 @@ var counter = 0;
 app.post('/', (request, response) => {
   counter++;
 
-  var modified = [];
+  var filelist = [];
 
   console.log("trigger ... " + counter);
 
@@ -48,49 +48,47 @@ app.post('/', (request, response) => {
       console.log ("modified : " + modified_files);
       console.log ("added : " + added_files);
       console.log ("removed : " + removed_files);
-
-      console.log(modified_files + " " + modified_files.length);
     }
 
-/*    if (modified_files.length > 0) {
-      filelist += modified_files;
+    var split_modified_files = modified_files.toString().split(',');
+    if (split_modified_files.length > 1) {
+      for (modified_counter = 0; modified_counter < split_modified_files.length; modified_counter++) {
+        console.log("M : pushing : " + split_modified_files[modified_counter]);
+        filelist.push(split_modified_files[modified_counter]);
+      }    
     }
-    if (added_files.length > 0) {
-      if (filelist.length > 0) {
-        filelist += "," + added_files;
-      } else {
-        filelist = added_files;
+
+    var split_added_files = added_files.toString().split(',');
+    if (split_added_files.length > 1) {
+      for (added_counter = 0; added_counter < split_added_files.length; added_counter++) {
+        console.log("A : pushing : " + split_added_files[added_counter]);
+        filelist.push(split_added_files[added_counter]);
       }
-    }
-    if (removed_files.length > 0) {
-      if (filelist.length > 0) {
-        filelist += "," + removed_files;
-      } else {
-        filelist = removed_files;
-      }
-    }
-*/
-
-
-    var split_filelist = filelist.toString().split(',');
-    for (modified_counter = 0; modified_counter < split_filelist.length; modified_counter++) {
-      modified.push(split_modified[modified_counter]);
-
-    }    
+    } 
+    var split_removed_files = removed_files.toString().split(',');
+    if (split_removed_files.length > 1) {
+     for (removed_counter = 0; removed_counter < split_removed_files.length; removed_counter++) {
+        console.log("R : pushing : " + split_removed_files[removed_counter]);
+        filelist.push(split_removed_files[removed_counter]);
+     }
+    }  
   }
 
   if (filterDebug) {
-    console.log(modified);
+    console.log("---------------------------------------------------------");
+    console.log("Files for comparison");
+    console.log("---------------------------------------------------------");
+    console.log(filelist);
   }
 
   match = false;
 
-  for (count = 0; count < modified.length; count++) {
+  for (count = 0; count < filelist.length; count++) {
     for (prefixCount = 0; prefixCount < prefixes.length; prefixCount++) {
       if (filterDebug) {
-        console.log ("compare : " + modified[count] + " with " + prefixes[prefixCount]);
+        console.log ("compare : " + filelist[count] + " with " + prefixes[prefixCount]);
       }
-      if (modified[count].startsWith(prefixes[prefixCount])) {
+      if (filelist[count].startsWith(prefixes[prefixCount])) {
         if (filterDebug) {
           console.log("matched");
         }
@@ -104,5 +102,4 @@ app.post('/', (request, response) => {
   response.send("ok" + "\n");
 });
 
-console.log("Listening on port " + port);
 app.listen(port, () => console.log("Listening on port " + port));
