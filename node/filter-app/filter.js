a0eea37fe8e8843eadf41799f9680b3f4ae28d0e
Mark Roberts <marrober@redhat.com>
2023-01-10 14:25:28 +0000

    Updates

--- node/filter-app/filter.js
+++ node/filter-app/filter.js
@@ -34,16 +34,27 @@ app.post('/', (request, response) => {
 
   console.log("trigger ... " + counter);
 
-  var jsonObj = JSON.stringify(request.body.commits);
+  var jsonCommitsObj = JSON.stringify(request.body.commits);
+  var jsonFullObj = JSON.stringify(request.body);
+
+  var fullObj = JSON.parse(jsonFullObj);
+
+  console.log("Repository : " + fullObj.repository.clone_url);
+
+  console.log("Pusher : " + fullObj.pusher.name);
 
   // console.log(request.body.commits);
 
-  var commits = JSON.parse(jsonObj);
+  var commits = JSON.parse(jsonCommitsObj);
 
   for (count = 0; count < commits.length; count++) {
     var modified_files= commits[count].modified;
     var added_files = commits[count].added;
     var removed_files = commits[count].removed;
+
+    console.log("Commit ID : " + commits[count].id);
+    console.log("Committer : " + commits[count].committer.name
+  );
     if (filterDebug) {
       console.log ("modified : " + modified_files);
       console.log ("added : " + added_files);
@@ -51,25 +62,37 @@ app.post('/', (request, response) => {
     }
 
     var split_modified_files = modified_files.toString().split(',');
-    if (split_modified_files.length > 1) {
+    if (split_modified_files.length > 0) {
       for (modified_counter = 0; modified_counter < split_modified_files.length; modified_counter++) {
-        console.log("M : pushing : " + split_modified_files[modified_counter]);
-        filelist.push(split_modified_files[modified_counter]);
+        if (split_modified_files[modified_counter].length > 0) {
+          if (filterDebug) {
+            console.log("M : pushing : " + split_modified_files[modified_counter]);
+          }
+          filelist.push(split_modified_files[modified_counter]);
+        }
       }
     }
 
     var split_added_files = added_files.toString().split(',');
-    if (split_added_files.length > 1) {
+    if (split_added_files.length > 0) {
       for (added_counter = 0; added_counter < split_added_files.length; added_counter++) {
-        console.log("A : pushing : " + split_added_files[added_counter]);
-        filelist.push(split_added_files[added_counter]);
+        if (split_added_files[added_counter].length > 0) {
+          if (filterDebug) {
+            console.log("A : pushing : " + split_added_files[added_counter]);
+          }
+          filelist.push(split_added_files[added_counter]);
+        }
       }
     }
     var split_removed_files = removed_files.toString().split(',');
-    if (split_removed_files.length > 1) {
+    if (split_removed_files.length > 0) {
      for (removed_counter = 0; removed_counter < split_removed_files.length; removed_counter++) {
-        console.log("R : pushing : " + split_removed_files[removed_counter]);
-        filelist.push(split_removed_files[removed_counter]);
+       if (split_removed_files[removed_counter].length > 0) {
+         if (filterDebug) {
+           console.log("R : pushing : " + split_removed_files[removed_counter]);
+         }
+         filelist.push(split_removed_files[removed_counter]);
+       }
      }
     }
   }
@@ -91,6 +114,8 @@ app.post('/', (request, response) => {
       if (filelist[count].startsWith(prefixes[prefixCount])) {
         if (filterDebug) {
           console.log("matched");
+        } else {
+          console.log("match on : " + filelist[count])
         }
         match = true;
       }
