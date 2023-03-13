d8e2ebac66e30f0050a04c96e7d0001a7f543ff3
Mark Roberts <mroberts@redhat.com>
2023-03-13 11:59:11 +0000

    Adding DevSpaces content

--- cpp/example-1/example1.cpp
+++ cpp/example-1/example1.cpp
@@ -1,20 +1,34 @@
 #include <string>
+#include <stdio.h>
 #include <iostream>
 
 class TestClass {       
-  public:             
-    int aNumber;        
-    std::string aString;  
+    public:             
+        int aNumber = 0;        
+        std::string aString = "";  
+    int getNumber() {
+        return aNumber;
+    }
+    std::string getString() {
+        return aString;
+    }
+    void setNumber(int newNumber) {
+        aNumber = newNumber;
+    }
+    void setString(std::string newString) {
+        aString = newString;
+    }
 };
 
 int main() {
     TestClass myTestClass;
 
-    myTestClass.aNumber = 3000;
-    myTestClass.aString = "Mark";
+    myTestClass.setNumber(3000);
+    myTestClass.setString("Mark");
 
-    std::cout << myTestClass.aNumber << "\n";
-    std::cout << myTestClass.aString << "\n";
+    std::cout << "Simple test app\n";
+    std::cout << "Number: " << myTestClass.getNumber() << "\n";
+    std::cout << "String: " << myTestClass.getString() << "\n";
 
     return(0);
 }
