ab964e85cfd41a924a4ad5bc438ed496ab761391
Mark Roberts <marrober@redhat.com>
2023-05-03 12:14:59 +0100

    updates

---
+++ node/app-1/layer.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const express = require('express');
+const sprintfJS = require('sprintf-js');
+const app = express();
+
+/* API endpoints ......................
+    /             - Get the IP address of the current layer.
+*/
+
+const port = 8080;
+
+console.log("Application is starting......");
+console.log("This app name : app-1");
+
+app.get('/', (request, response) => {
+  messageText = sprintfJS.sprintf("This is app-1");
+  console.log("", messageText);
+  response.send(messageText + "\n");
+});
+
+app.listen(port, () => console.log("phase: setup", "Listening on port " + port));
+
