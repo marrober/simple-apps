a70cb8a3c9d1e7e9589f373eb7c18b2917919c94
Mark Roberts <marrober@redhat.com>
2023-05-05 15:59:24 +0100

    fix

--- node/multi-apps/app-2/src/layer.js
+++ node/multi-apps/app-2/src/layer.js
@@ -3,17 +3,17 @@ const express = require('express');
 const sprintfJS = require('sprintf-js');
 const app = express();
 
-/* API endpoints ......................
+/* API endpoints ........................
     /             - Get the IP address of the current layer.
 */
 
 const port = 8080;
 
 console.log("Application is starting......");
-console.log("This app name : app-2");
+console.log("This app name : app-1");
 
 app.get('/', (request, response) => {
-  messageText = sprintfJS.sprintf("This is app-2");
+  messageText = sprintfJS.sprintf("This is app-1");
   console.log("", messageText);
   response.send(messageText + "\n");
 });
